4ebf570b22aea4a57087c9743c97ce6cb010a074
tracer-nju <tracer@nju.edu.cn>
2023-11-09 21:24:08 -0500

    Linux Ubuntu-22-04 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:24:08 up 14:51,  0 users,  load average: 0.16, 0.42, 0.40

--- .shadow/abstract-machine/klib/src/stdio.c
+++ .shadow/abstract-machine/klib/src/stdio.c
@@ -7,7 +7,37 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 int printf(const char *fmt, ...) {
-  panic("Not implemented");
+  va_list args;
+  va_start(args, fmt);
+
+  int count = 0;
+  while (*fmt) {
+    if (*fmt == '%') {
+      fmt++;
+      if (*fmt == 'd') {
+        int num = va_arg(args, int);
+        putch(num + '0');  // convert the integer to its ascii character representation for printing.
+
+        count++;
+      } else if (*fmt == 's') {
+        char *str = va_arg(args, char *);
+        while (*str) {
+          putch(*str);
+          str++;
+          count++;
+        }
+        count++;
+      }
+    } else {
+      putch(*fmt);
+
+      count++;
+    }
+    fmt++;
+  }
+
+  va_end(args);
+  return count;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
