6bd4833c4d4b97dbd4d714db3b86cff78da1b707
tracer-nju <tracer@nju.edu.cn>
2023-11-12 03:57:36 -0500

    Linux Ubuntu-22-04 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  03:57:36 up 26 min,  1 user,  load average: 1.79, 1.52, 1.16

--- .shadow/kernel/kernel.c
+++ .shadow/kernel/kernel.c
@@ -42,18 +42,18 @@ void print_key() {
 
 // draw a tile(w*h) with color
 #ifndef DRAW_TILE_H_
-#define DRAW_TILE_H_
-static void draw_tile(int x, int y, int w, int h, uint32_t color) {
-  uint32_t pixels[w * h]; // WARNING: large stack-allocated memory
-  AM_GPU_FBDRAW_T event = {
-    .x = x, .y = y, .w = w, .h = h, .sync = 1,
-    .pixels = pixels,
-  };
-  for (int i = 0; i < w * h; i++) {
-    pixels[i] = color;
-  }
-  ioe_write(AM_GPU_FBDRAW, &event);
-}
+// #define DRAW_TILE_H_
+// static void draw_tile(int x, int y, int w, int h, uint32_t color) {
+//   uint32_t pixels[w * h]; // WARNING: large stack-allocated memory
+//   AM_GPU_FBDRAW_T event = {
+//     .x = x, .y = y, .w = w, .h = h, .sync = 1,
+//     .pixels = pixels,
+//   };
+//   for (int i = 0; i < w * h; i++) {
+//     pixels[i] = color;
+//   }
+//   ioe_write(AM_GPU_FBDRAW, &event);
+// }
 #endif
 
 void splash() {
