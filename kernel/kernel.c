89fe220d8fc1c94eb9bd353d9b4a85e01d98e370
GQY479 <gqy4790@outlook.com>
2023-11-13 09:08:45 -0500

    draw lines

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -3,8 +3,10 @@
 #include <klib.h>
 #include <klib-macros.h>
 #include "image_data.h"
+#include "./graphics/draw_tile.h"
+#include "./graphics/draw_line.h"
 
-#define SIDE 1
+#define SIDE 8
 
 static int w, h;  // Screen size
 
@@ -38,7 +40,15 @@ void print_key() {
   }
 }
 
+void get_w_h(){
+  AM_GPU_CONFIG_T info = {0};
+  ioe_read(AM_GPU_CONFIG, &info);
+  w = info.width;
+  h = info.height;
+}
 // draw a tile(w*h) with color
+#ifndef DRAW_TILE_H_
+#define DRAW_TILE_H_
 static void draw_tile(int x, int y, int w, int h, uint32_t color) {
   uint32_t pixels[w * h]; // WARNING: large stack-allocated memory
   AM_GPU_FBDRAW_T event = {
@@ -50,6 +60,7 @@ static void draw_tile(int x, int y, int w, int h, uint32_t color) {
   }
   ioe_write(AM_GPU_FBDRAW, &event);
 }
+#endif
 
 void splash() {
   AM_GPU_CONFIG_T info = {0};
@@ -109,16 +120,23 @@ void draw_image(const unsigned char* image_data, int image_width, int image_heig
 int main(const char *args) {
   ioe_init();
 
+  get_w_h();
+
   puts("mainargs = \"");
   puts(args);  // make run mainargs=xxx
   puts("\"\n");
 
-  unsigned char* image_data = hair_flowing;
-  int image_width = 2268;
-  int image_height = 1200;
-
-  if(1)
+  if(0){
+    unsigned char* image_data = hair_flowing;
+    int image_width = 2268;
+    int image_height = 1200;
     draw_image(image_data, image_width, image_height);
+  }
+  
+  draw_line(0, 0, w, h, 0xffffff, 0, SIDE, 1);
+  draw_line(0, h, w, 0, 0xffffff, 0, 1, 1);
+  draw_line(0, 0, w-100, h-100, 0xff0000, 0, 8, 2);
+  draw_line(50, 50, 300, 600, 0x0000ff, 1, 8, 3);
   // splash();
 
   puts("Press any key to see its key code...\n");
