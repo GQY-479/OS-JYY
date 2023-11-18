8e93b8cdf306dc3a6273e019ca9d6c630b1186b4
GQY479 <gqy4790@outlook.com>
2023-11-18 13:55:39 -0500

    improve printf() with print_int

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -6,8 +6,61 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+void print_int(int num) {
+  if (num == 0) {
+    putch('0');
+    return;
+  }
+
+  if (num < 0) {
+    putch('-');
+    num = -num;
+  }
+
+  char digits[10];
+  int i = 0;
+
+  while (num > 0) {
+    digits[i] = num % 10 + '0';
+    num /= 10;
+    i++;
+  }
+
+  for (int j = i - 1; j >= 0; j--) {
+    putch(digits[j]);
+  }
+}
+
 int printf(const char *fmt, ...) {
-  panic("Not implemented");
+  va_list args;
+  va_start(args, fmt);
+
+  int count = 0;
+  while (*fmt) {
+    if (*fmt == '%') {
+      fmt++;
+      if (*fmt == 'd') {
+        int num = va_arg(args, int);
+        print_int(num);
+        count++;
+      } else if (*fmt == 's') {
+        char *str = va_arg(args, char *);
+        while (*str) {
+          putch(*str);
+          str++;
+          count++;
+        }
+        count++;
+      }
+    } else {
+      putch(*fmt);
+      count++;
+    }
+    fmt++;
+  }
+
+  va_end(args);
+  return count;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
