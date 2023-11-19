a2f5abfe06fe3d9e621c0a311ee62a80bd773441
tracer-nju <tracer@nju.edu.cn>
2023-11-19 01:00:25 -0500

    Linux Ubuntu-22-04 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:00:25 up  3:48,  1 user,  load average: 2.79, 1.68, 1.41

--- .shadow/kernel/graphics/draw_arrow.h
+++ .shadow/kernel/graphics/draw_arrow.h
@@ -2,17 +2,17 @@
 #include "draw_line.h"
 // #include <math.h>
 
-#define sqrt(x) __builtin_sqrt(x)
+#define sqrt3 1.732
 
 void draw_arrow(int x1, int y1, int x2, int y2, uint32_t color, int bold, int pixel_side) {
     draw_line(x1, y1, x2, y2, color, bold, pixel_side, 1);
     int dx = x2 - x1, dy = y2 - y1;
     dx = -dx; dy = -dy;
     // rotate 30 degree
-    int dx1 = sqrt(3)/2 * dx - 1/2 * dy;
-    int dy1 = 1/2 * dx + sqrt(3)/2 * dy;
-    int dx2 = sqrt(3)/2 * dx + 1/2 * dy;
-    int dy2 = -1/2 * dx + sqrt(3)/2 * dy;
+    int dx1 = sqrt3/2 * dx - 1/2 * dy;
+    int dy1 = 1/2 * dx + sqrt3/2 * dy;
+    int dx2 = sqrt3/2 * dx + 1/2 * dy;
+    int dy2 = -1/2 * dx + sqrt3/2 * dy;
     
     // float len = sqrt(dx*dx + dy*dy);
     dx1 = dx1 / 4; dy1 = dy1 / 4;
